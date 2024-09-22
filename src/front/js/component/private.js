f7d6f4ff5d9e12929b769cd6ebcc6273497ff321
Davinia pd <delgadavi@gmail.com>
2024-09-22 21:20:08 +0000

    posts en el contexto

--- src/front/js/component/private.js
+++ src/front/js/component/private.js
@@ -15,35 +15,7 @@ export const Private = () => {
 		return <Navigate to="/" />;
 	}
 
-	const posts = [
-		{
-			profileImage: "https://i.pinimg.com/originals/57/00/c0/5700c04197ee9a4372a35ef16eb78f4e.png",
-			title: "Game of Thrones",
-			image: "https://img.freepik.com/premium-photo/set-futuristic-gaming-chairs-16-bit-pixel-with-led-lights-sl-game-asset-design-concept-art_655090-1872057.jpg",
-			altText: "Gamer chair collage",
-			likedBy: ["corsair", "razer", "ikea", "50,000 others"],
-			content: "Once you start coding, you won't be able to stop. So I'm going to give you some advice: Choose a good throne",
-			hashtag: "#forthefuture #gameofthrones"
-		},
-		{
-			profileImage: "https://i.pinimg.com/originals/57/00/c0/5700c04197ee9a4372a35ef16eb78f4e.png",
-			title: "You are not alone",
-			image: "https://static.vecteezy.com/system/resources/previews/034/545/064/non_2x/concept-of-cyber-crime-or-cyber-attack-graphic-of-skull-with-programming-script-background-vector.jpg",
-			altText: "A skull on a computer screen",
-			likedBy: ["4geeksacademy", "mentalhealthfoundation", "80,000 others"],
-			content: "It is normal to feel frustrated while coding. Don't give up",
-			hashtag: "#youarenotalone"
-		},
-		{
-			profileImage: "https://i.pinimg.com/originals/57/00/c0/5700c04197ee9a4372a35ef16eb78f4e.png",
-			title: "Coding like a woman",
-			image: "https://imgcdn.stablediffusionweb.com/2024/5/17/e7fac0a3-bee9-4e6f-b45e-5626875e53cf.jpg",
-			altText: "A woman using a computer",
-			likedBy: ["68.322 likes"],
-			content: "\"For most a history, Anonymous was a woman\"", 
-			hashtag: "#VirginiaWoolf #womencoding"
-		}
-	];
+	const posts = store.posts;
 
 	return (
 		<>
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -14,6 +14,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { ProfileTab } from "./component/profileTab";
 
 //create your first component
 const Layout = () => {
@@ -33,8 +34,8 @@ const Layout = () => {
                         <Route element={<Signup />} path="/signup" />
                         <Route element={<Login />} path="/login" />
                         <Route element={<Private />} path="/private" />
+                        <Route element={<ProfileTab />} path="/profile/:theid" />
                         <Route element={<Logout />} path="/logout" />
-                        <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
